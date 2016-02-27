3da55133c1a6bf9a070fe7894e99dd2b63cdbb56
Autumn Fjeld <autumn@ziploop.com>
2016-02-27 14:19:56 -0800

    Successful fetch to api with screen_name param, tweet file cleanup

--- app/javascript/app.js
+++ app/javascript/app.js
@@ -12,14 +12,14 @@ define([
       el: $("#app-container"),
 
       events: {
-       "keypress #twitter-screen-name":  "createOnEnter",
+       "keypress #screen-name-input":  "createOnEnter",
       },
 
       initialize: function() {
         console.log('App.initialize');
         _.bindAll(this, 'render');
 
-        this.input = this.$("#twitter-screen-name");
+        this.input = this.$("#screen-name-input");
       },
 
       render: function() {
@@ -28,8 +28,21 @@ define([
 
       createOnEnter: function(e) {
         if (e.keyCode != 13) return;
-        console.log('input was', this.input.val());
-        Tweets.fetch();
+        var screenName = this.input.val();
+        console.log('input was', screenName);
+        Tweets.fetch({
+          data: {screen_name: screenName},
+          success: success,
+          error:  error
+        });
+        //Either listen to a “change” event on the model or provide a “success” callback in the fetch method, to know when the data has been returned so that you can load it in to your view and display it as needed.
+        function success(model, response){
+          console.log('SUCCESS: Tweets.fetch', model, response);
+        }
+
+        function error(err){
+          console.log('ERROR: Tweets.fetch', err);
+        }
       }
 
     });
--- app/javascript/tweets/tweet.model.js
+++ app/javascript/tweets/tweet.model.js
@@ -1,18 +1,11 @@
-// define(['underscore', 'backbone'], function(_, Backbone) {
-//   var TweetModel = Backbone.Model.extend({
+define(['underscore', 'backbone'], function(_, Backbone) {
+  var TweetModel = Backbone.Model.extend({
 
-//     // Default tweet attributes
-//     defaults: {
-//       id_str: "111111111111",
-//       user: {screen_name: 'henri'},
-//       text: 'woo hoo! learning backbone!'
-//     },  
+    initialize: function(){
+      console.log('TweetModel initialized')
+    },
 
-//     initialize: function(){
-//       console.log('TweetModel initialized')
-//     },
+  });
 
-
-//   });
-//   return TweetModel;
-// });
+  return TweetModel;
+});
