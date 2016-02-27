5e2422a3bec6e79014e222baf03a407e906c8ab4
Autumn Fjeld <autumn@ziploop.com>
2016-02-27 12:55:33 -0800

    Update backbone paths

--- app/javascript/main.js
+++ app/javascript/main.js
@@ -2,7 +2,7 @@
 require.config({
   paths: {
     jquery: '../../node_modules/jquery/dist/jquery.min',
-    underscore: '../../node_modules/backbone/node_modules/underscore/underscore-min',
+    underscore: '../../node_modules/underscore/underscore-min',
     backbone: '../../node_modules/backbone/backbone-min'
   }
 
@@ -11,6 +11,5 @@ require.config({
 require([
   'app',
 ], function(App){
-  console.log('App', App);
   var app = new App();
 });
