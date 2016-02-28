da38453a459e6769ffa1ecfae7200bc2977ad292
Autumn Fjeld <autumn@ziploop.com>
2016-02-28 13:18:08 -0800

    Cleanup TweetModel

--- app/javascript/tweets/tweet.model.js
+++ app/javascript/tweets/tweet.model.js
@@ -1,10 +1,6 @@
 define(['underscore', 'backbone'], function(_, Backbone) {
   var TweetModel = Backbone.Model.extend({
 
-    initialize: function(){
-      console.log('TweetModel initialized')
-    },
-
   });
 
   return TweetModel;
