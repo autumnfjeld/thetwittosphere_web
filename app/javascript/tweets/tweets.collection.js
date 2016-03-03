c83a727c4c649fd0ae47d442b963146d6a31a1d5
Autumn Fjeld <autumn@ziploop.com>
2016-03-02 16:46:10 -0800

    TweetCollection: add filter function

--- app/javascript/tweets/tweets.collection.js
+++ app/javascript/tweets/tweets.collection.js
@@ -6,11 +6,18 @@ define(['underscore', 'backbone', 'tweets/tweet.model'],
 
     // Reference to this collection's model.
     model: Tweet,
-    url: "http://localhost:3000/api/usertimeline"
+    url: "http://localhost:3000/api/usertimeline",
+
+    filterByRetweet: function (value) {  //{retween_min: integer , picture: boolean }
+      var results = this.models.filter(function(model){
+        return model.get('retweet_count') >= value;
+      })
+      results = _.map( results, function( model ) { return model.toJSON()  } );
+
+      return new Backbone.Collection(results);
+    }     
 
   });
 
   return new TweetsCollection;
 });
-
-
