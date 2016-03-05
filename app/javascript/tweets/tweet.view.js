73a9afd3edbaa60cae6ff3efd062ae9862d3945f
Autumn Fjeld <autumn@ziploop.com>
2016-03-04 16:26:16 -0800

    Adapt tweet view for development

--- app/javascript/tweets/tweet.view.js
+++ app/javascript/tweets/tweet.view.js
@@ -2,8 +2,9 @@ define(['jquery', 'underscore', 'backbone'],
   function($, _, Backbone){
     var TweetView = Backbone.View.extend({
 
-      //basic test to get api data to view
-      template: _.template("<p class='tweet-text'> <%= text %> </p>"),
+      el: "<div class='tweet-box'></div>",
+      //maybe reformt retweet layout, use retweet icon
+      template: _.template( "<p class='tweet-text'> <%= text %> &nbsp; rt:<%= retweet_count %> </p>"),
 
       initialize: function(){
         _.bindAll(this, 'render');
@@ -11,9 +12,29 @@ define(['jquery', 'underscore', 'backbone'],
       },
 
       render: function(){
+        // console.log('a tweet', this.model.toJSON());
         $(this.el).html(this.template(this.model.toJSON()));
+        this.addImage(this.model.toJSON());
         return this;
+      },
+
+      addImage: function(model){
+        var  mediaUrl = model.entities.media && model.entities.media[0].media_url_https; 
+        // console.log('mediaUrl +++', mediaUrl);
+        $(this.el).append("<div>" + mediaUrl + "</div>");
       }
+      // viewData: function(model){
+      //   // console.log('model', model);
+      //   var viewModel = {
+      //     text: model.text,
+      //     retweet_count: model.retweet_count,
+      //     media_url: model.entities.media && model.entities.media[0].media_url_https || "hi"
+      //   }
+      //   if (model.entities.media) {
+      //     viewModel.media_url = model.entities.media[0].medial_url_https
+      //   }
+      //   return viewModel;
+      // }
 
     });
 
