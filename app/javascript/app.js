3a4c1f5d2808fadf0552fb84cc64c965dd7f9d61
Autumn Fjeld <autumn@ziploop.com>
2016-03-04 16:27:47 -0800

    App: Add updateTweetView to connect UI filter inputs to collection filtering and TweetView rendering

--- app/javascript/app.js
+++ app/javascript/app.js
@@ -11,21 +11,21 @@ define([
   ], function($, _, Backbone, Tweets, TweetView, Profile, ProfileView){
     var AppView = Backbone.View.extend({
 
-
       // bind to existing app container
       el: $("#app-container"),
 
       events: {
        "keypress #screen-name-input":  "fetchTwitInfo",
-       "keypress #retweet-filter-input":  "updateTweetView"
+       "change #retweet-filter-input":  "updateTweetView",
+       "change #picture-select": "updateTweetView"
       },
 
       initialize: function() {
         _.bindAll(this, 'render', 'addProfileView', 'addTweetView', 'updateTweetView');
 
         this.input = this.$("#screen-name-input");
-        this.filterByRetweet = this.$("#retweet-filter-input")
-        this.filterByPicutre = this.$("#retweet-filter-input")
+        this.filterByRetweet = this.$("#retweet-filter-input");
+        this.filterByPicutre = this.$("#picture-select");
 
         Tweets.bind('add', this.addTweetView);
         Profile.bind('sync', this.addProfileView);
@@ -38,7 +38,6 @@ define([
       fetchTwitInfo: function(e) {
         if (e.keyCode != 13) return;
         var screenName = this.input.val();
-        console.log('input was', screenName);
 
         Profile.fetch({
           data: {screen_name: screenName},
@@ -61,26 +60,34 @@ define([
       },
 
       addProfileView: function(profile){
-        var profileView = new ProfileView({model: profile});
-        console.log('addProfileView', profileView);
-        this.$("#user-profile-box").append(profileView.render().el);
+        var profileId = profile.get('id');
+        if (this.$("#user-profile-box").attr("data-id") !== profileId){
+          this.$("#tweet-list-box").empty();  
+          var profileView = new ProfileView({model: profile});
+          this.$("#user-profile-box").append(profileView.render().el);
+        }
       },
 
-      //should filtering be done at view or in collection?
       addTweetView: function(tweet){
+        // this.$("#tweet-list-box").empty();  //temporary handling of repeat entried
         var tweetView = new TweetView({model: tweet});
         this.$("#tweet-list-box").append(tweetView.render().el);
       },
 
       // Add all items in the **Todos** collection at once.
-      updateTweetView: function() {
-        var minRetweet = this.filterByRetweet.val();  //assume people are looking for more-than
-        console.log('updateTweetView', minRetweet);
-        if(minRetweet) {
+      updateTweetView: function(e) {
+        // if (e.type === 'keypress' && e.keyCode != 13) return;        
+        var minRetweets = this.filterByRetweet.val() || 0,  //assume people are looking for more-than
+            hasPicture = this.filterByPicutre.val();
+        if (hasPicture === 'null') hasPicture = null;
+        console.log('updateTweetView:: minRetweets', minRetweets, 'hasPicture', hasPicture, typeof hasPicture);
+
+        if(minRetweets || hasPicture) {
           //get filtered collection
-          var filteredTweets = Tweets.filterByRetweet(minRetweet);
+          var filteredTweets = Tweets.filter(minRetweets, hasPicture);
+          console.log('filteredTweets length:', filteredTweets.length);
           console.log('filteredTweets', filteredTweets);
-          //brute force: re-display whole list, wouldn't be practicle for long list, then would be nice to remove individual tweets
+          //brute force: re-display whole list, might not be practicle for long list, then would be nice to remove/add individual tweets
           this.$("#tweet-list-box").empty();     
           filteredTweets.each(this.addTweetView);  
         }
